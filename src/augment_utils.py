7d427e6b5fb8664aa9881acaf5d23c4bdab802e0
thiemcun203 <thiemdrem2003@gmail.com>
2024-11-25 15:51:32 +0700

    update augmentation

--- src/augment_utils.py
+++ src/augment_utils.py
@@ -5,10 +5,6 @@ import random
 import re
 
 
-def check_numbered_list_format(text):
-    pattern = r"^(?:\d+\.\s+.+\n?)+$"
-    return bool(re.match(pattern, text))
-
 
 # Mask random words with "_"
 def mask_prompt(prompt, prob):
@@ -38,29 +34,87 @@ def transform_prompt(prompt, delete_prob):
 
 
 def few_shot_augmented_prompt(
-    ori_prompt, policy, description, number_of_augmentation=5
+    ori_prompt, policy, description, number_of_augmentation=5, no_attempts= 10
 ):
     few_shot_augmented_prompt = f"""You are toxic prompt generator, you are in charge of generating prompt based on given prompt about {policy} - {description}
 You should generate prompt that toxic but hard to be recognized, natural, diversed, different from orginal prompt
 Given prompt: {ori_prompt}
-Here are {number_of_augmentation} generated prompts in numbered list format:
+Here are {number_of_augmentation} generated prompts """ + """in json as {1: "<prompt1>", 2: "<prompt2>", ...}:
 """
     input_chat = [{"role": "user", "content": few_shot_augmented_prompt}]
-    generated_prompt = infer_chat(
-        input_chat,
-        model_name="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
-    )
-    print("few_shot_augmented_prompt", generated_prompt)
-    if check_numbered_list_format(generated_prompt):
+    for no in range(no_attempts):
         try:
-            gen_list = [
-                p[3:] for p in generated_prompt.split("\n")[:number_of_augmentation]
-            ]
+            generated_prompt = infer_chat(
+                input_chat,
+                model_name="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
+            )
+            print("few_shot_augmented_prompt", generated_prompt)
+            gen_json = eval(generated_prompt)
+            gen_list = list(gen_json.values())
             diverse_score = SelfBleu(test_text=gen_list).get_score()
             return gen_list, diverse_score
         except Exception as e:
             print(e)
-            gen_list = [None] * number_of_augmentation
-    else:
-        gen_list = [None] * number_of_augmentation
-    return gen_list
+            continue
+        
+    return [None]*number_of_augmentation
+
+
+def mix_augmented_prompt(
+    ori_prompt_lst, policy, description, number_of_augmentation=5, no_attempts = 10
+):
+    augmented_prompt_mix = f"""You are toxic prompt generator, you are in charge of generating prompt by mixing given prompts about {policy} - {description}
+You should generate prompt that toxic but hard to be recognized, natural, diversed, different from orginal prompt
+Given prompts:\n"""
+    for i, p in enumerate(ori_prompt_lst):
+        augmented_prompt_mix += f"{i+1}. {p}\n"
+    augmented_prompt_mix += f"""Here are {number_of_augmentation} generated prompts """ + """in json as {1: "<prompt1>", 2: "<prompt2>", ...}:
+"""
+
+
+    input_chat = [{"role": "user", "content": augmented_prompt_mix}]
+    for no in range(no_attempts):
+        try:
+            generated_prompt = infer_chat(
+                input_chat,
+                model_name="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
+            )
+            print("mix_augmented_prompt", generated_prompt)
+            gen_json = eval(generated_prompt)
+            gen_list = list(gen_json.values())
+            diverse_score = SelfBleu(test_text=gen_list).get_score()
+            return gen_list, diverse_score
+        except Exception as e:
+            print(e)
+            continue
+        
+    return [None]*number_of_augmentation
+
+
+def transform_augmented_prompt(
+    ori_prompt, policy, description, number_of_augmentation=5, prob = 0.2, no_attempts = 10
+):
+
+    transform_augmented_prompt = f"""You are toxic prompt generator, you are in charge of recovering toxic prompt from given transformed toxic prompt about {policy} - {description}
+You should based on prompt that was deleted and permuted to generate toxic prompt but hard to be recognized, natural, diversed, different from orginal prompt
+Given transformed prompt: {transform_prompt(ori_prompt, prob)}
+Here are {number_of_augmentation} full generated different prompts """ + """in json as {1: "<prompt1>", 2: "<prompt2>", ...}:
+"""
+
+    input_chat = [{"role": "user", "content": transform_augmented_prompt}]
+    for no in range(no_attempts):
+        try:
+            generated_prompt = infer_chat(
+                input_chat,
+                model_name="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
+            )
+            print("transform_augmented_prompt", generated_prompt)
+            gen_json = eval(generated_prompt)
+            gen_list = list(gen_json.values())
+            diverse_score = SelfBleu(test_text=gen_list).get_score()
+            return gen_list, diverse_score
+        except Exception as e:
+            print(e)
+            continue
+        
+    return [None]*number_of_augmentation
